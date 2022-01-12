69d1c9e3a6ffa9cd55240393e26979fb43e4d9c0
Артемий <66246727+Ferotyy@users.noreply.github.com>
2022-01-12 12:18:16 +0300

    Update Exceptions.java

--- src/lab4/Exceptions.java
+++ src/lab4/Exceptions.java
@@ -2,7 +2,7 @@ package lab4;
 
 
 public class Exceptions {
-    public static class NotPresent extends ArithmeticException{
+    public class NotPresent extends ArithmeticException{
         public NotPresent(String s){
             super(s);
         }
